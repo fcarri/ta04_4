f28cb2b615e258e8a1576b988627c6aa665a5f9f
fcarri <fcs0000@gmail.com>
2022-10-04 17:00:31 +0200

    Commit 7

---
+++ ta04_4.java
@@ -0,0 +1,17 @@
+package project7;
+
+public class ta04_4 {
+	public static void main(String args[]){
+		
+		int N = 0;
+		N = N + 77;
+		System.out.println("N = "+N);
+		
+		N = N - 3;
+		System.out.println("N = "+N);
+		
+		N = N * 2;
+		System.out.println("N = "+N);
+		
+	}
+}
